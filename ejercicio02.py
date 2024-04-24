40378a2e0265b3032bd7f3d4a05ddbe10d78ec38
jose apaza saavedra <jos123apaza@gmail.com>
2024-04-23 21:44:45 -0400

    subimos el archivo ejercicio02.py

---
+++ ejercicio02.py
@@ -0,0 +1,13 @@
+#variables
+name = "jose"
+last_name = "apaza saavedra"
+age = 28
+altura = 1.73
+like = ["futbol","helado","jugo"]
+person_info = {
+    'name':"jose",
+    'last_name': "apaza",
+    'age':28
+}
+
+print(type(person_info))
