160d5ceadd0540cff6fd589303a8786922b819b7
jose apaza saavedra <jos123apaza@gmail.com>
2024-04-23 21:28:12 -0400

    pasamos el archivo ejericio01.py

---
+++ ejercicio01.py
@@ -0,0 +1,13 @@
+print("hello world")
+print(12)
+print(2 + 3)
+print(3 - 2)
+print(3 * 4)
+print(10 ** 2)
+
+#type of data
+print(type("hola"))
+print(type(12))
+print(type(1.72))
+print(type(True))
+print(type([1,2,3,4,5]))
