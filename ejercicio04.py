e3bdefdc100eb36aeed4f3ed90c569e2a2b40ea7
jose apaza saavedra <jos123apaza@gmail.com>
2024-04-25 22:03:04 -0400

    agregamos los archivos ejercicio04.py y ejercicio05.py

---
+++ ejercicio04.py
@@ -0,0 +1,9 @@
+#********string (CADENAS)**********
+
+full_name = "JOSE APAZA SAAVEDRA"
+
+print( full_name.isupper() )
+
+#**SPLIT***
+name = "jose apaza saavedra 12 juan josue wilmer ruben"
+print( len(name.split()) )
---
+++ ejercicio05.py
@@ -0,0 +1,26 @@
+#********LIT*************
+
+frutas = ["uva","frutilla","platano"]
+asistencia = ["jose","josue","ruben","antonio","wilmer","jose"]
+paises = ["haiti","españa","china"]
+edades = [28, 25, 18, 15]
+tallas = [1.73, 1.65, 1.80]
+
+asistencia.append("logan")#agregando un registro a la lista
+asistencia.append("juan")#agregando un registro a la lista
+asistencia.insert(2,"porfirio") #insertar un registro a la posicion deseada
+
+asistencia.remove("wilmer") #eliminar un registro de la lista
+asistencia.remove("jose") 
+asistencia.pop() #eliminar los registro de la lista (del ultimo)
+asistencia.pop(1) #eliminar la posicion de la lista
+del asistencia[2] #eliminar un registro de la lista segun la posicion
+
+asistencia.clear() #vaciar la lista
+
+join = frutas + paises #unir listas
+
+frutas.sort() #ordena la lista de forma ascendente A- Z
+paises.sort(reverse=True) #ordenar la listra de forma descendente Z - A
+
+print( paises )
