5e504362286887afc22ae39b47d1b5caead83bbc
jose <jos123apaza@gmail.com>
2024-05-17 15:59:03 -0400

    audio

---
+++ 5. PROYECTOS/traductor.py
@@ -0,0 +1,36 @@
+#pip install SpeechRecognition
+import tkinter
+import speech_recognition as sr
+
+class App:
+    def __init__(self):
+        self.ventana = tkinter.Tk()
+        self.ventana.title("PROYECTO TRADUCTOR DE AUDIO A TEXTO")
+        self.ventana.geometry('500x300')
+        
+        self.recognizer = sr.Recognizer()
+        self.audio_file = sr.AudioFile('./nota.wav')
+
+        self.boton1 = tkinter.Button( self.ventana, text="traducir", command=self.traducir )
+        self.boton1.grid( column=0, row=0 )
+
+        self.label1 = tkinter.Label( self.ventana, text="" )
+        self.label1.grid( column=0,row=1 )
+
+        self.ventana.mainloop()
+    
+    def traducir(self):
+        with self.audio_file as source:
+            self.audio_data = self.recognizer.record(source)
+
+        # Usar el reconocedor para convertir audio a texto
+        try:
+            self.text = self.recognizer.recognize_google(self.audio_data, language='es-ES')
+            self.label1.config(text=self.text)
+        except sr.UnknownValueError:
+            self.label1.config(text="No se pudo conectar el audio...")
+        except sr.RequestError:
+            self.label1.config(text="Error al conectarse con el servicio de reconocimiento de voz")
+
+#**********************
+app = App()
